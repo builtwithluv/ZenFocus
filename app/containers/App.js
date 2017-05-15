12e06be34d5a48e21b227d1a4c78ab83ba045e9c
Cheng Ly <chengsieuly@gmail.com>
2017-05-14 21:43:12 -0700

    feat: Use dark theme

--- app/containers/App.js
+++ app/containers/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <div className="pt-dark">
         {this.props.children}
       </div>
     );
