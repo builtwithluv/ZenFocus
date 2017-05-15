022106828f4dc2f999aad76fa80ff72bd5bb2740
Cheng Ly <chengsieuly@gmail.com>
2017-05-14 17:44:09 -0700

    feat: Show arrows on time picker

--- app/components/CountdownTimer/index.js
+++ app/components/CountdownTimer/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { TimePicker } from '@blueprintjs/datetime';
 
-export default class CountdownTimer extends React.Component {
+export default class CountdownTimer extends Component {
   render() {
-    return <TimePicker />;
+    return (
+      <TimePicker
+        showArrowButtons
+      />
+    );
   }
 }
 
