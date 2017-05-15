d99db39c90d6f12ad48dfabfb90a431dabd8bd97
Cheng Ly <chengsieuly@gmail.com>
2017-05-14 21:14:18 -0700

    feat: Show only the minute and seconds with blueprint timer

--- app/components/CountdownTimer/countdown-timer.js
+++ app/components/CountdownTimer/countdown-timer.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { TimePicker } from '@blueprintjs/datetime';
+import { TimePicker, TimePickerPrecision } from '@blueprintjs/datetime';
 
 export default class CountdownTimer extends Component {
   render() {
     return (
       <TimePicker
         showArrowButtons
+        precision={TimePickerPrecision.SECOND}
       />
     );
   }
