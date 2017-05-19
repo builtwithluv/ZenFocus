a398cc8dbf83aa9ba5ffbf3dba2a3f8e323b633c
Cheng Ly <chengsieuly@gmail.com>
2017-05-19 07:34:24 -0700

    feat: Add slider for charts

--- app/components/Charts/charts.js
+++ app/components/Charts/charts.js
@@ -1,28 +1,59 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import { RangeSlider } from '@blueprintjs/core';
 import LineGraph from '../common/LineGraph';
 import {
   getDate
 } from '../../utils/date.util';
 
 export default class Charts extends PureComponent {
+  constructor() {
+    super();
+    this.onResizeWindow = () => this.forceUpdate();
+    this.state = {
+      range: [1, 30]
+    };
+  }
+
+  componentWillMount() {
+    window.addEventListener('resize', this.onResizeWindow);
+  }
+
   componentDidMount() {
     const { loadChartData } = this.props;
     loadChartData();
   }
 
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.onResizeWindow);
+  }
+
+  onSliderChange(range) {
+    this.setState({ range });
+  }
+
   render() {
-    const { data } = this.props;
+    const { range } = this.state;
     const defaultData = [{
       date: getDate,
       focusLength: 0,
       shortBreakLength: 0,
       rounds: 0,
     }];
+    let { data } = this.props;
+    data = data.slice(data.length - 30);
+    data = data.slice(range[0] - 1, range[1]);
 
     return (
-      <div className="container-fluid vh-100 bg-dark-gray-5">
+      <div className="charts container-fluid vh-100 bg-dark-gray-5">
         <LineGraph data={data.length < 1 ? defaultData : data} />
+        <RangeSlider
+          min={1}
+          max={30}
+          value={range}
+          onChange={(val) => this.onSliderChange(val)}
+          className="mt-5"
+        />
       </div>
     );
   }
