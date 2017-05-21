3db1306220c65e7f8282a14508471841dd012b3b
Cheng Ly <chengsieuly@gmail.com>
2017-05-20 20:26:18 -0700

    build: Load dev store

--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -1,6 +1,10 @@
-// @flow
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./configureStore.prod'); // eslint-disable-line global-require
-} else {
-  module.exports = require('./configureStore.dev'); // eslint-disable-line global-require
-}
+/*
+  Use later in real production so it's easier to test with production
+*/
+// if (process.env.NODE_ENV === 'production') {
+//   module.exports = require('./configureStore.prod'); // eslint-disable-line global-require
+// } else {
+//   module.exports = require('./configureStore.dev'); // eslint-disable-line global-require
+// }
+
+module.exports = require('./configureStore.dev'); // eslint-disable-line global-require
