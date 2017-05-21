13975ca9a2df892a2f674108b514520acffb872c
Cheng Ly <chengsieuly@gmail.com>
2017-05-20 23:34:43 -0700

    Revert "build: Load dev store"
    This reverts commit 3db1306220c65e7f8282a14508471841dd012b3b.

--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -1,10 +1,6 @@
-/*
-  Use later in real production so it's easier to test with production
-*/
-// if (process.env.NODE_ENV === 'production') {
-//   module.exports = require('./configureStore.prod'); // eslint-disable-line global-require
-// } else {
-//   module.exports = require('./configureStore.dev'); // eslint-disable-line global-require
-// }
-
-module.exports = require('./configureStore.dev'); // eslint-disable-line global-require
+// @flow
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./configureStore.prod'); // eslint-disable-line global-require
+} else {
+  module.exports = require('./configureStore.dev'); // eslint-disable-line global-require
+}
