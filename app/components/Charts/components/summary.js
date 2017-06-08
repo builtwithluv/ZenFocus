aaabe9e9ca9d81aa345a8d07cc81744aa639f600
Hoon Choi <vhchoi@gmail.com>
2017-06-08 01:46:30 -0700

    Refactor: Using built-in methods for data manipulation

--- app/components/Charts/components/summary.js
+++ app/components/Charts/components/summary.js
@@ -1,22 +1,35 @@
 import React from 'react';
-import _ from 'lodash';
 
 const Summary = ({ data }) => {
-  const sessions = _.keys(data).length;
-  const focusLength = _.reduce(data, (sum, session) => sum + session.focusLength, 0);
-  const longBreakLength = _.reduce(data, (sum, session) => sum + session.longBreakLength, 0);
-  const shortBreakLength = _.reduce(data, (sum, session) => sum + session.shortBreakLength, 0);
+  const { fontStyle, fslDiv, sessionDiv } = styles;
+  const sessions = Object.keys(data).length;
+  const focusLength = data.reduce((sum, session) => sum + session.focusLength, 0);
+  const longBreakLength = data.reduce((sum, session) => sum + session.longBreakLength, 0);
+  const shortBreakLength = data.reduce((sum, session) => sum + session.shortBreakLength, 0);
 
   return (
     <nav>
-      <div className="pt-navbar-group pt-align-center" style={{fontSize:"13"}}>
-        <div style={{margin:"0 auto"}}>Sessions: {sessions}</div>
-        <div style={{marginLeft:"10", margin: "0 auto"}}>Total Focus: {focusLength ? focusLength : 0} minutes</div>
-        <div style={{marginLeft:"10", margin: "0 auto"}}>Total Short Break: {shortBreakLength ? shortBreakLength : 0} minutes</div>
-        <div style={{marginLeft:"10", margin: "0 auto"}}>Total Long Break: {longBreakLength ? longBreakLength : 0} minutes</div>
+      <div className="pt-navbar-group pt-align-center" style={fontStyle}>
+        <div style={sessionDiv}>Sessions: {sessions}</div>
+        <div style={fslDiv}>Total Focus: {focusLength ? focusLength : 0} minutes</div>
+        <div style={fslDiv}>Total Short Break: {shortBreakLength ? shortBreakLength : 0} minutes</div>
+        <div style={fslDiv}>Total Long Break: {longBreakLength ? longBreakLength : 0} minutes</div>
       </div>
     </nav>
   );
 }
+
+const styles = {
+  fontStyle: {
+    fontSize: "13"
+  },
+  fslDiv: {
+    marginLeft: "10",
+    margin: "0 auto"
+  },
+  sessionDiv: {
+    margin: "0 auto"
+  }
+};
 
 export default Summary;
