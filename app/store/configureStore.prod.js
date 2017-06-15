da1f4b6f17830bddc581333c3fdaa9e36daab147
Cheng Ly <chengsieuly@gmail.com>
2017-06-15 07:35:14 -0700

    feat: Add sentry analytics

--- app/store/configureStore.prod.js
+++ app/store/configureStore.prod.js
@@ -1,12 +1,21 @@
+import { remote } from 'electron';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { createBrowserHistory } from 'history';
 import { routerMiddleware } from 'react-router-redux';
+import RavenMiddleware from 'redux-raven-middleware';
 import rootReducer from '../reducers';
 
 const history = createBrowserHistory();
 const router = routerMiddleware(history);
-const enhancer = applyMiddleware(thunk, router);
+const enhancer = applyMiddleware(
+  RavenMiddleware('https://d08845737ecd42cd836268b6ca8c1ab9@sentry.io/180237', {
+    release: remote.app.getVersion(),
+    environment: 'production'
+  }),
+  thunk,
+  router
+);
 
 function configureStore(initialState) {
   return createStore(rootReducer, initialState, enhancer);
