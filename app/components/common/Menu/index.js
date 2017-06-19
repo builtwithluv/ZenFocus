4ca02e0affb4255b1897842b6a4b3e437835d0a5
Cheng Ly <chengsieuly@gmail.com>
2017-06-19 10:24:06 -0700

    fix: Menu theme when changing theme

--- app/components/common/Menu/index.js
+++ app/components/common/Menu/index.js
@@ -2,9 +2,13 @@ import { connect } from 'react-redux';
 import Menu from './menu';
 import { resetRound, resetSession } from '../Rounds/actions';
 
+const mapStateToProps = state => ({
+  theme: state.app.theme
+});
+
 const mapDispatchToProps = dispatch => ({
   resetRound: () => dispatch(resetRound()),
   resetSession: () => dispatch(resetSession())
 });
 
-export default connect(null, mapDispatchToProps)(Menu);
+export default connect(mapStateToProps, mapDispatchToProps)(Menu);
