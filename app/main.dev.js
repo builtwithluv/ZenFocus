43afee022105db7408144beaf7b446b87f4039f0
Cheng Ly <chengsieuly@gmail.com>
2017-07-08 10:58:53 -0700

    refactor: Building tray

--- app/main.dev.js
+++ app/main.dev.js
@@ -1,9 +1,9 @@
 import path from 'path';
-import os from 'os';
-import { app, Tray } from 'electron';
+import { app } from 'electron';
 import { installExtensions } from './electron/utils';
 import buildMain from './electron/main';
 import buildMenu from './electron/menu';
+import buildTray from './electron/tray';
 import updater from './electron/updater';
 import setAppListeners from './electron/listeners';
 
@@ -21,10 +21,8 @@ if (
   require('module').globalPaths.push(p); // eslint-disable-line global-require
 }
 
-const PLATFORM = os.platform();
-
 let mainWindow = null;
-let tray = null;
+let tray = null; // eslint-disable-line no-unused-vars
 
 app.on('window-all-closed', () => {
   app.quit();
@@ -39,19 +37,9 @@ app.on('ready', async () => {
   }
 
   mainWindow = buildMain(`file://${__dirname}/app.html`);
+  tray = buildTray(mainWindow);
 
   buildMenu(mainWindow);
-  buildTray(mainWindow);
   updater(mainWindow);
   setAppListeners(mainWindow);
 });
-
-function buildTray(win) {
-  const icon = PLATFORM === 'darwin' || PLATFORM === 'linux'
-    ? path.join(__dirname, 'assets/images/icon-mac@2x.png')
-    : path.join(__dirname, 'assets/images/icon-windows@2x.png');
-
-  tray = new Tray(icon);
-
-  tray.on('click', () => win.isVisible() ? win.hide() : win.show());
-}
