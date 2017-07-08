c71c71f345ac875650780f9e64d85376d873f928
Cheng Ly <chengsieuly@gmail.com>
2017-07-08 08:27:35 -0700

    feat: Add bare minimal tray

--- app/main.dev.js
+++ app/main.dev.js
@@ -1,6 +1,6 @@
 import path from 'path';
 import os from 'os';
-import { app, BrowserWindow, ipcMain } from 'electron';
+import { app, BrowserWindow, ipcMain, nativeImage, Tray } from 'electron';
 import settings from 'electron-settings';
 import { installExtensions, setWindowSize } from './electron/utils';
 import buildMenu from './electron/menu';
@@ -24,6 +24,8 @@ if (
 const PLATFORM = os.platform();
 
 let mainWindow = null;
+let tray = null;
+
 app.on('window-all-closed', () => {
   app.quit();
 });
@@ -72,5 +74,18 @@ app.on('ready', async () => {
 
   setWindowSize(mainWindow, settings.get('system.compact'));
   buildMenu(mainWindow);
+  buildTray(mainWindow);
   updater(mainWindow);
 });
+
+function buildTray(win) {
+  const windowsTrayIcon = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABz0lEQVR42n2TO0gkQRCGu8fZVBAPFE00Ok0UzlBDQQRNFFfEQPGRyMFdqCYGYmJkZuAll8kmF12gkYGggXDoBiIGPlBET1DZ0cXZnR2/gmppZHHg46/urv67pqfGpGnqE6j6ZD7LeQ8qlUoAoo0wCYvM/4VTyDGehq+aE1QzyKBjaB5iqDD2SZh6gR++iV/aCjzDoygJEbqP/kavxQ8uiMV8QU1q3OYxkOcWjmEevkCo699IXkqS5AA9ZPwf7fIr2FGDOxhwl1Xo7zfF5WVL/ZbNcuIUnLN2ieacQRPcq8Ev7+at3o2MQ41b0V30BE7L5XKbLHZCrAazYJKzszDKZs3z6GgDNEdDQybO5ZzhOvqPig7jOB4Ug25I1OAnmHI+n2GjgTboFLN4ezvUi5Occ/ShVCoNi0EHvKjBFhioEXXIRsq1YDh5jnEEKfGsS9pL9WHhu86Fr5ubQdTXZ6+MMXGxGBQKBTFaI+eG9VW01xkM6v5X1SlXCUkWXEWtxI+wAVmoe+9t+c5aQVG7cBFp8V6jHj1C96FHX918/EEmWHzSjhuBJuJ2mJfmgT/EtdrGFjXV/rJGaNFThRlttHGvL6yiBlDl9q1qvdfSgZt3vAFFsTmJw9JBOgAAAABJRU5ErkJggg==';
+  const macTrayIcon = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAACCElEQVR42l2Sv2tTURTHz315Lz8xmB8mtQaaSi0aeGoWHUMQpIuL0abQRBdrxoLgUFdxkTr4B7gpOOjmoOjmJI0gCGID0sUl+BNplZbq83PhXHjtFz6cm3PP+d6bc5/sk8nlchJXNps1sleeMUaUvRtgkyW4yHIJHsN7eABd8nVX6/u+WNSFjOf5hAvwBrbgL0TwT+M2fIFr2uOJU6PRELQCP+AbfIWf8ALuwQcO2KVxZPeJy9qaENVlPeUzvIWbFBVSqZTRvzWbSCQGtVptRG5IagPD0xLTKzX4BHNuoHfC0FydnvaCctlM1etSKpU6GHxk7x08ElUFxmpwX3MBGGQH5a5qE5OEJ8QhRuvFYvGYoBOwrUNbBGkWCv7vblc25+ercGRnYUFucxs1WSU8z+fza61WqyPoDOxCBAOQc9VqQKPA8a1u9xTIYGbG3eI6YS2dTo/b7XZPUAM21eCZmy6FkkwmxUYL1zWVSkUymUyPAW6Q2+GjuyKq1xDFb4H8u82mF/V6Jur3ZXJiIhGGoZTL5VWM7WvdwOisqOa0+Y/GvuY9FH/vGs85tnOANnsH4l/ULYjY+KUDXaagymmiKsAQXpI/CUdB7NvGTRbhO6xjdJ6CIidOsV4iN4KHQRBkMDWsjTVwQ4qbHOL3YVGxvkR4Cp1YDtHjZJ04yf1XJ1dxEHxX6jqd/gMs/WoVZImhVAAAAABJRU5ErkJggg==';
+  const icon = PLATFORM === 'darwin' || PLATFORM === 'linux'
+    ? nativeImage.createFromDataURL(macTrayIcon)
+    : nativeImage.createFromDataURL(windowsTrayIcon);
+
+  tray = new Tray(icon);
+
+  tray.on('click', () => win.isVisible() ? win.hide() : win.show());
+}
