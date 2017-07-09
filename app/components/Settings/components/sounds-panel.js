40ee9b2db73d6434460913f6c6b0810983d263b9
Cheng Ly <chengsieuly@gmail.com>
2017-07-08 20:02:30 -0700

    style: Show tabs for sound type selection

--- app/components/Settings/components/sounds-panel.js
+++ app/components/Settings/components/sounds-panel.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Checkbox } from '@blueprintjs/core';
+import { Checkbox, Tab2, Tabs2 } from '@blueprintjs/core';
 import { getAllSounds } from '../../utils/sounds.util';
 import { Phases } from '../../enums';
 
-const TickSoundOption = ({
+const SoundOption = ({
   label,
   selectedSound,
   sounds,
@@ -23,124 +23,146 @@ const TickSoundOption = ({
   </div>
 );
 
-TickSoundOption.propTypes = {
+SoundOption.propTypes = {
   label: PropTypes.string.isRequired,
   selectedSound: PropTypes.number.isRequired,
   sounds: PropTypes.arrayOf(PropTypes.any),
   onChange: PropTypes.func.isRequired,
 };
 
-const SoundsPanel = ({
-  audioPhaseDisabled,
-  audioTickDisabled,
+const TickPanel = ({
   sounds,
   soundFocusPhase,
   soundShortBreakPhase,
   soundLongBreakPhase,
   soundPhaseEnded,
   onSettingsChange,
-  toggleAudioPhase,
-  toggleAudioTick,
-  setAudio
+  setAudio,
 }) => (
-  <div className="mt-1">
-    <div className="mb-3">
-      <TickSoundOption
-        label="Focus Tick"
-        selectedSound={soundFocusPhase}
-        sounds={sounds}
-        onChange={e => {
-          onSettingsChange(
-            'sounds.focusPhase',
-            +e.target.value,
-            setAudio,
-            Phases.FOCUS
-          );
-        }}
-      />
-      <TickSoundOption
-        label="Short Break Tick"
-        selectedSound={soundShortBreakPhase}
-        sounds={sounds}
-        onChange={e => {
-          onSettingsChange(
-            'sounds.shortBreakPhase',
-            +e.target.value,
-            setAudio,
-            Phases.SHORT_BREAK
-          );
-        }}
-      />
-      <TickSoundOption
-        label="Long Break Tick"
-        selectedSound={soundLongBreakPhase}
-        sounds={sounds}
-        onChange={e => {
-          onSettingsChange(
-            'sounds.longBreakPhase',
-            +e.target.value,
-            setAudio,
-            Phases.LONG_BREAK
-          );
-        }}
-      />
-      <TickSoundOption
-        label="Phase Ended Tick"
-        selectedSound={soundPhaseEnded}
-        sounds={sounds}
-        onChange={e => {
-          onSettingsChange(
-            'sounds.phaseEnded',
-            +e.target.value,
-            setAudio
-          );
-        }}
-      />
-    </div>
-    <div>
-      <label className="pt-label">Volume Control</label>
-      <Checkbox
-        label="Play ticking sound"
-        checked={!audioTickDisabled}
-        onChange={e =>
-          onSettingsChange(
-            'sounds.audioTickDisabled',
-            !e.target.checked,
-            toggleAudioTick
-          )
-        }
-      />
-      <Checkbox
-        label="Play sound when phase ends"
-        checked={!audioPhaseDisabled}
-        onChange={e =>
-          onSettingsChange(
-            'sounds.audioPhaseDisabled',
-            !e.target.checked,
-            toggleAudioPhase
-          )
-        }
-      />
-    </div>
+  <div>
+    <SoundOption
+      label="Focus Tick"
+      selectedSound={soundFocusPhase}
+      sounds={sounds}
+      onChange={e => {
+        onSettingsChange(
+          'sounds.focusPhase',
+          +e.target.value,
+          setAudio,
+          Phases.FOCUS
+        );
+      }}
+    />
+    <SoundOption
+      label="Short Break Tick"
+      selectedSound={soundShortBreakPhase}
+      sounds={sounds}
+      onChange={e => {
+        onSettingsChange(
+          'sounds.shortBreakPhase',
+          +e.target.value,
+          setAudio,
+          Phases.SHORT_BREAK
+        );
+      }}
+    />
+    <SoundOption
+      label="Long Break Tick"
+      selectedSound={soundLongBreakPhase}
+      sounds={sounds}
+      onChange={e => {
+        onSettingsChange(
+          'sounds.longBreakPhase',
+          +e.target.value,
+          setAudio,
+          Phases.LONG_BREAK
+        );
+      }}
+    />
+    <SoundOption
+      label="Phase Ended Tick"
+      selectedSound={soundPhaseEnded}
+      sounds={sounds}
+      onChange={e => {
+        onSettingsChange(
+          'sounds.phaseEnded',
+          +e.target.value,
+          setAudio
+        );
+      }}
+    />
   </div>
 );
 
-SoundsPanel.defaultProps = {
-  sounds: Array.from(getAllSounds())
-};
-
-SoundsPanel.propTypes = {
-  audioPhaseDisabled: PropTypes.bool.isRequired,
-  audioTickDisabled: PropTypes.bool.isRequired,
+TickPanel.propTypes = {
   soundFocusPhase: PropTypes.number.isRequired,
   soundShortBreakPhase: PropTypes.number.isRequired,
   soundLongBreakPhase: PropTypes.number.isRequired,
   soundPhaseEnded: PropTypes.number.isRequired,
   sounds: PropTypes.arrayOf(PropTypes.any),
   onSettingsChange: PropTypes.func.isRequired,
+  setAudio: PropTypes.func.isRequired,
+};
+
+const VolumnControl = ({
+  audioPhaseDisabled,
+  audioTickDisabled,
+  onSettingsChange,
+  toggleAudioPhase,
+  toggleAudioTick,
+  className
+}) => (
+  <div className={className}>
+    <label className="pt-label">Volume Control</label>
+    <Checkbox
+      label="Play ticking sound"
+      checked={!audioTickDisabled}
+      onChange={e =>
+        onSettingsChange(
+          'sounds.audioTickDisabled',
+          !e.target.checked,
+          toggleAudioTick
+        )
+      }
+    />
+    <Checkbox
+      label="Play sound when phase ends"
+      checked={!audioPhaseDisabled}
+      onChange={e =>
+        onSettingsChange(
+          'sounds.audioPhaseDisabled',
+          !e.target.checked,
+          toggleAudioPhase
+        )
+      }
+    />
+  </div>
+);
+
+VolumnControl.propTypes = {
+  audioPhaseDisabled: PropTypes.bool.isRequired,
+  audioTickDisabled: PropTypes.bool.isRequired,
+  onSettingsChange: PropTypes.func.isRequired,
   toggleAudioPhase: PropTypes.func.isRequired,
   toggleAudioTick: PropTypes.func.isRequired,
-  setAudio: PropTypes.func.isRequired
+  className: PropTypes.string
+};
+
+const SoundsPanel = (props) => (
+  <div>
+    <Tabs2 id="SoundType" animate={false}>
+      <Tab2
+        id="soundTypeTick"
+        title="Tick"
+        panel={<TickPanel {...props} />}
+      />
+    </Tabs2>
+    <VolumnControl className="mt-4" {...props} />
+  </div>
+);
+
+SoundsPanel.defaultProps = {
+  sounds: Array.from(getAllSounds())
 };
 
 export default SoundsPanel;
