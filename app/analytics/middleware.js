bc6e8a523f6005450b6468fc19d5931d3b53cf2e
Cheng Ly <chengsieuly@gmail.com>
2017-07-21 08:27:58 -0700

    analytics: Fix duplicate users shown

--- app/analytics/middleware.js
+++ app/analytics/middleware.js
@@ -2,14 +2,24 @@
 
 import Analytics from 'electron-google-analytics';
 import { remote } from 'electron';
+import settings from 'electron-settings';
+import uuidv4 from 'uuid/v4';
 
-const analytics = new Analytics('UA-101155103-1');
+const TRACKING_ID = 'UA-101155103-1';
+
+let clientID = settings.get('clientID');
+if (!clientID) {
+  clientID = uuidv4();
+  settings.set('clientID', clientID);
+}
+
+const analytics = new Analytics(TRACKING_ID);
 
 export const analyticsLogger = store => next => action => { // eslint-disable-line no-unused-vars
   switch (action.type) {
     case '@@router/LOCATION_CHANGE': {
       const path = action.payload.pathname;
-      analytics.event('View', 'click', { evLabel: path });
+      analytics.event('View', 'click', { clientID, evLabel: path });
       break;
     }
 
